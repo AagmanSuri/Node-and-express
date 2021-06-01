74ab127771bc157d285faff386e5a4cc61a62f57
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-06-01 23:40:37 +0530

    added for diff url (routes)

--- server1.js
+++ server1.js
@@ -3,4 +3,44 @@ import express from 'express'
 
 const app= express();
 
+app.get('/',(req,res)=>{
+    const user=[
+        {
+            name:'aagman',
+            age:20
+        },
+        {
+            name:'suri',
+            age:20
+        },
+        {
+            name:'fake',
+            age:20
+        },
+    ]
+    res.send(user);
+})
+app.get('/profile',(req,res)=>{
+    const user=[
+        {
+            name:'aagman',
+            age:20,
+            profile:"job"
+            
+        },
+        {
+            name:'suri',
+            age:20,
+            profile:"job"
+        },
+        {
+            name:'fake',
+            age:20,
+            profile:"job"
+        },
+    ]
+    res.send(user)
+})
+
+
 app.listen(3000);
